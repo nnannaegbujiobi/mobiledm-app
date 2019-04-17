10c76c0accba37ec72d2ebb27f6d914980eb1232
nnannaegbujiobi <nnanna@newvybe.com>
2019-04-16 20:50:27 -0500

    Adding files.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app" >
+  <div class="animal">
+    <div id="box">
+      <h1>Tamagotchi 3000</h1>
+         <h3>Hours alive:{{timer}}</h3>
+        <p> {{moods[life]}}</p>
+  
+    <button v-on:click="addTime"> Wait One Hour </button> <br>
+    <button v-on:click="addPet">Pet</button>
   </div>
+  </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data()  {
+    return {
+      timer: 0, 
+      life: 4,
+      moods: ['😧','🙁','😐','🙂','😍'],
+      pet: 0
+    };
+  },
+   
+  methods: {
+    addTime() {
+      var timePast = this.timer;
+      this.timer += 1 ;
+      if (this.life >= 1) {
+        if (timePast < this.timer && this.pet === 0) {
+          this.life -= 1;
+        } else if (timePast < this.timer && this.pet > 1) {
+          this.life += 1;
+        }
+      }
+      this.pet = 0;
+    }, 
+      
+    addPet() {
+      this.pet += 1;
+
+    }
+
+  },
+    
+
+
+};
+
+
 </script>
+
+
+<style scoped>
+
+
+  #box {
+    background-color: ##ffffff;
+    padding: 250px;
+    margin: 20px;
+    margin-bottom: 50px;
+    outline: solid black;
+    height: 30
+  }
+
+  img {
+    margin-top: 50px;
+    margin-right: 50px;
+  }
+  
+  p {
+    font-size: 10em;
+  }
+    
+
+</style>
