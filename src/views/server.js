86eaea011615796cb59620bc351bf9983f8c9446
nnannaegbujiobi <nnanna@newvybe.com>
2019-04-16 23:05:26 -0500

    final version

--- src/views/server.js
+++ src/views/server.js
@@ -1,9 +1,7 @@
-
-
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var app = express();
+var  app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
